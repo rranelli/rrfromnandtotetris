6a491208a373638857e17ae30abe8841e83ae499
renan-ranelli <renanranelli@gmail.com>
2015-04-23 03:10:26 -0300

    Add helper function with defuns for testing

---
+++ defuns.el
@@ -0,0 +1,29 @@
+(add-hook 'vhdl-mode-hook
+          (lambda ()
+            (define-key vhdl-mode-map (kbd "C-c , c") 'rr/nand2tetris-simulate-and-compare)))
+
+(add-to-list 'auto-mode-alist '(".hdl$" . vhdl-mode))
+
+(defun rr/nand2tetris-simulate-and-compare ()
+  (interactive)
+  (rr/nand2tetris-simulate-hdl)
+  (rr/nand2tetris-compare-out))
+
+(defun rr/nand2tetris-simulate-hdl ()
+  (interactive)
+  (->> (buffer-file-name)
+       (replace-regexp-in-string ".hdl$" ".tst")
+       (format
+        "%snand2tetris/tools/HardwareSimulator.sh %s"
+        (projectile-project-root))
+       (shell-command)))
+
+(defun rr/nand2tetris-compare-out ()
+  (interactive)
+  (let ((out-file (replace-regexp-in-string ".hdl$"
+                                            ".out"
+                                            (buffer-file-name)))
+        (cmp-file (replace-regexp-in-string ".hdl$"
+                                            ".cmp"
+                                            (buffer-file-name))))
+    (diff out-file cmp-file)))
