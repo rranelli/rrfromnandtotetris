325fe591c9479be7d9e4baeadac3838bd515e10f
renan-ranelli <renanranelli@gmail.com>
2015-06-07 00:43:31 -0300

    Writing my own multiplication procedure ;)

--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -7,3 +7,45 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R2
+M=0 // initialize result to be zero
+
+@R0 // jump to the end if the value of R0 is zero
+D=M
+
+@END
+D;JEQ
+
+@R1 // jump to the end if the value of R1 is zero
+D=M
+@END
+D;JEQ
+
+@R1 // store the value of R1 into rone
+D=M
+@rone
+M=D
+
+(LOOP)
+  @R2 // set D to the result so far
+  D=M
+
+  @R0 // add R0 to D
+  D=D+M
+
+  @R2 // write back the result of the sum to R2
+  M=D
+
+  @rone // decrement the value of `rone`
+  M=M-1
+  D=M // grap the value of `rone` to check if the loop should continue
+
+  @END
+  D=D-1;JLT // if `rone` is now -1 that means we have to stop the loop,
+
+  @LOOP
+  0;JMP // Back to the beginning
+(END)
+
+@END
+0;JMP
