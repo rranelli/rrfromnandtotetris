8199d7be91e3438b618ad32a7149f35fa0c5a4e2
renan-ranelli <renanranelli@gmail.com>
2015-06-13 02:16:46 -0300

    Maybe this is enough for week4 ??

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -1,11 +1,58 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+	// This file is part of www.nand2tetris.org
+	// and the book "The Elements of Computing Systems"
+	// by Nisan and Schocken, MIT Press.
+	// File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, the
-// program clears the screen, i.e. writes "white" in every pixel.
+	// Runs an infinite loop that listens to the keyboard input.
+	// When a key is pressed (any key), the program blackens the screen,
+	// i.e. writes "black" in every pixel. When no key is pressed, the
+	// program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+	// Put your code here.
+	(LOOP) // beginning of the infinite loop
+          @KBD //grabs the keyboard input at D
+          D = M
+
+          @color
+          M = 0
+          @white
+          D; JEQ
+
+          @0
+          D = !A // all ones in binary
+          @color
+          M = D
+          (white)
+
+          @8192
+          D = A
+          @i
+          M = D
+
+          (INNER)
+            @i // check if we should paint the next pixel
+            D = M
+            @END
+            D; JLT
+
+            @SCREEN // set A to the address of the current pixel
+            D = A
+            @i
+            D = D + M
+            @p
+            M = D
+
+            @color // set the pixel to the current color
+            D = M
+            @p
+            A = M
+            M = D
+
+            @i // increment i
+            M = M - 1
+
+            @INNER // if we should not, just jump to start
+            0; JEQ
+          (END)
+        @LOOP
+        0; JEQ
